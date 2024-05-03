7f0aecc4601b41e24ebd93ab254bcb353a950cdb
Colin243 <312colinho@gmail.com>
2024-05-03 14:55:04 -0700

    adding spotipy

--- test.py
+++ test.py
@@ -11,19 +11,17 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import matplotlib.pyplot as plt
 import numpy
+import pygame
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
+scope = "user-library-read"
 
-# RED = (255, 0, 0)
-# GREEN = (0, 255, 0)
-# BLUE = (0, 0, 255)
-# y, sr = librosa.load('Zedd - Clarity (feat. Foxes).wav')
-# tempo, beatframes = librosa.beat.beat_track(y=y, sr=sr)
-# print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
-# print(tempo)
-# beat_times = librosa.frames_to_time(beatframes, sr=sr)
-# print(beat_times)
-length = 271
-print (length / 128)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+print(sp.search('BEVERLY', limit=1))
+#print(sp.audio_features('https://open.spotify.com/track/03hl08vyDaBCQxQr8X6HXg?si=b9867a04d8994add')[0]['tempo'])
 
 
 
